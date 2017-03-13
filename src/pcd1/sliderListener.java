b507bb831e7ec965322f9d04786bd43bf095900a
irs <irsalina12778@gmail.com>
2017-03-13 14:16:59 +0700

    tugas pcd

---
+++ src/pcd1/sliderListener.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pcd1;
+
+/**
+ *
+ * @author Thosiba
+ */
+class sliderListener {
+
+    public sliderListener() {
+    }
+    
+}
